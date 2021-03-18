053f7e110cdee1ce4054f3a37fd9be225b5de32f
WilliamMacDonald <25089571+Sum112358@users.noreply.github.com>
2021-03-18 18:27:29 -0400

    added data and updated model

--- scripts/buildModel.py
+++ scripts/buildModel.py
@@ -4,6 +4,7 @@ from joblib import dump
 
 import os
 import numpy as np
+import random
 
 class UVCharacteristics:
     date: str = None
@@ -32,12 +33,17 @@ def getIndex(p: UVCharacteristics):
         x,y = t
         return (abs(x-y),x)
 
-    INDEXRANGES = {100:1, 1000:4, 400:2, 780:3, 40:0}
-    if p.polarized:
-        p.mean = p.mean*2.2 
+    INDEXRANGES = {100:1, 1000:4, 400:2, 780:3, 40:0, 1800:5}
     
     out = min((map(errorCalc, [(x,p.mean) for x in INDEXRANGES.keys()])))
     return INDEXRANGES[out[1]]
+
+def correctPolarized(p, data):
+    if p.polarized:
+        data = [i*2.2 for i in data]
+        p.polarized = False
+        p.mean = np.mean(data)
+    return (p,data)
 
 def buildTrainingSet(location):
     bigData = []
@@ -46,6 +52,8 @@ def buildTrainingSet(location):
     for file in os.listdir(directory):
         if os.fsdecode(file).startswith('polarized'):
             indexParam, data = retreiveData(f"{location}/{os.fsdecode(file)}", polarized=True)
+            indexParam, data = correctPolarized(indexParam, data)
+
         else:
             indexParam, data = retreiveData(f"{location}/{os.fsdecode(file)}", polarized=False)
         
@@ -54,16 +62,17 @@ def buildTrainingSet(location):
     return (bigData, labels)
 
 def forceUniformity(data):
-    maxDataSize = min(map(len,data))
+    maxDataSize = 3200
     for i in range(len(data)):
         if len(data[i]) > maxDataSize:
+            data[i] = [random.choice(data[i]) for _ in range(maxDataSize)]
             data[i] = data[i][:maxDataSize]
     return (data, maxDataSize)
 
 def printData(UVIntensity, labels):
-    for i in range(len(UVIntensity)):
-        print(f"Average UV Index: {np.mean(UVIntensity[i])}")
-        print(f"Current Label: {labels[i]}")
+    #for i in range(len(UVIntensity)):
+    #    print(f"Average UV Index: {np.mean(UVIntensity[i])}")
+    #    print(f"Current Label: {labels[i]}")
     plt.plot([np.mean(i) for i in UVIntensity], labels, 'bo')
     plt.title("UV Index vs Reading")
     plt.xlabel("GPIO IN")
@@ -74,7 +83,7 @@ def execute_model():
     regr = svm.SVR()
     train_data, train_labels = buildTrainingSet("./scripts/data")
     train_data_uniform, training_size = forceUniformity(train_data)
-    #printData(train_data, train_labels)
+    printData(train_data, train_labels)
 
     regr.fit(train_data_uniform, train_labels)
     return (regr, training_size)
@@ -84,6 +93,7 @@ regr, maxSize = execute_model()
 if __name__ == "__main__":
     regr, maxSize = execute_model()
     # Save the model to disk
+    print(maxSize)
     dump(regr, 'UVIndexModel.joblib')
     # set the Local ENV MODELSIZE to the maxmodel Size
     open('MAXMODELSIZE', 'w').write(str(maxSize))
